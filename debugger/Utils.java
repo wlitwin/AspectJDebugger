0ffb3f0e904fc8311bce28eef92229ac1e37e2fe
Walter Litwinczyk <wtl4112@rit.edu>
2013-11-19 18:06:52 -0500

    Added comments

--- debugger/Utils.java
+++ debugger/Utils.java
@@ -1,6 +1,17 @@
 package debugger;
 
-class Utils {
+/**
+ * Contains some helper functions to be used by the entire
+ * debugger package.
+ */
+public class Utils {
+	/**
+	 * Determine if a string can be converted to a number
+	 *
+	 * @param str The string to check
+	 *
+	 * @return True if str can successfully be converted
+	 */
 	public static boolean isInteger(String str) {
 		try {
 			Integer.parseInt(str);
