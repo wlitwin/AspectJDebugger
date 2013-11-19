ab198700663586b71d7479640d8e4cbe8f6541e0
Walter Litwinczyk <wtl4112@rit.edu>
2013-11-19 17:59:38 -0500

    Added comments and things

--- debugger/CondBreakpoint.java
+++ debugger/CondBreakpoint.java
@@ -2,28 +2,44 @@ package debugger;
 
 import java.util.*;
 import java.lang.reflect.*;
-import java.util.regex.Pattern;
 
+/**
+ * Handles conditional expressions. It also provides the implementation
+ * for two debugger commands: BreakCondCommand and BreakCondRemoveCommand
+ */
 public class CondBreakpoint {
-
-	static {
-		Debugger.commands.add(new BreakCondCommand());
-		Debugger.commands.add(new BreakCondRemove());
-	}
-
 	private Expr root;
 	private String combinedName;
+
+	/**
+	 * Stores a parsed conditional expression and the class and method name.
+	 *
+	 * @param combinedName The class and method name in the format: class.name
+	 *
+	 * @param root A parsed (and valid) conditional expression
+	 */
 	public CondBreakpoint(String combinedName, Expr root) {
 		this.root = root;
 		this.combinedName = combinedName;
 	}
 
+	/**
+	 * Evaluate the expression with the given arguments. Call this to determine
+	 * if a breakpoint should happen.
+	 *
+	 * @param args The parameters to the method
+	 *
+	 * @return True if the expression evaluated to true, false otherwise
+	 */
 	public boolean evaluate(Object[] args) {
 		return (Boolean)root.evaluate(args);
 	}
 
+	//=========================================================================
+	//=========================================================================
+
 	/**
-	 *
+	 * Implementation for the command to create a new conditional breakpoint.
 	 */
 	static class BreakCondCommand implements ICommand {
 		public boolean matches(String input) {
@@ -57,7 +73,13 @@ public class CondBreakpoint {
 		}
 	}
 
-	static class BreakCondRemove implements ICommand {
+	//=========================================================================
+	//=========================================================================
+
+	/**
+	 * Implementation of the command to remove an existing breakpoint.
+	 */
+	static class BreakCondRemoveCommand implements ICommand {
 		public boolean matches(String input) {
 			return input.toLowerCase().equals("breakifremove");
 		}
@@ -156,6 +178,9 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Greater than expression, true if compareTo returns 1
+	 */
 	static class GtExpr extends BinExpr {
 		public GtExpr(Expr l, Expr r) {
 			super(Token.GT, l, r);
@@ -169,6 +194,10 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Greater than or equal to expression, true if compareTo
+	 * returns 0 or 1
+	 */
 	static class GteExpr extends BinExpr {
 		public GteExpr(Expr l, Expr r) {
 			super(Token.GTE, l, r);
@@ -182,6 +211,9 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Less than expression, return true if compreTo returns -1
+	 */
 	static class LtExpr extends BinExpr {
 		public LtExpr(Expr l, Expr r) {
 			super(Token.LT, l, r);
@@ -195,6 +227,10 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Less than or equal to expression, returns true if compareTo
+	 * returns -1 or 0
+	 */
 	static class LteExpr extends BinExpr {
 		public LteExpr(Expr l, Expr r) {
 			super(Token.LTE, l, r);
@@ -208,6 +244,9 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Equal expression, returns true if compareTo returns 0
+	 */
 	static class EqExpr extends BinExpr {
 		public EqExpr(Expr l, Expr r) {
 			super(Token.EQ, l, r);
@@ -221,6 +260,10 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Argument expression, returns an object from the args array
+	 * using the saved index
+	 */
 	static class ArgExpr extends Expr {
 		int index;
 		public ArgExpr(int index) {
@@ -233,6 +276,9 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Integer value, holds an integer constant
+	 */
 	static class IntExpr extends Expr {
 		int value;
 		public IntExpr(int value) {
@@ -245,6 +291,9 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * String value, holds a string constant
+	 */
 	static class StrExpr extends Expr {
 		String value;
 		public StrExpr(String value) {
@@ -257,6 +306,9 @@ public class CondBreakpoint {
 		}
 	}
 
+	/**
+	 * Null expression, represents null
+	 */
 	static class NullExpr extends Expr {
 		public NullExpr() { 
 			super(Token.NULL);
@@ -283,11 +335,15 @@ public class CondBreakpoint {
 	}
 
 	//=========================================================================
+	//=========================================================================
 
+	/**
+	 * These are the tokens that make up the mini conditional language
+	 */
 	enum Token {
-		AND("and", true, false, 0),
+		AND("and", true, false, 0), // Lowest precedence
 		OR("or",   true, false, 0),
-		EQ("=",    true, false, 1),
+		EQ("=",    true, false, 1), // Highest precedence
 		LT("<",    true, false, 1),
 		LTE("<=",  true, false, 1),
 		GT(">",    true, false, 1),
@@ -305,6 +361,7 @@ public class CondBreakpoint {
 		private boolean isBinOp;
 		private boolean isValue;
 		private int precedence;
+
 		Token(String s, boolean binop, boolean value, int precedence) {
 			this.value = s;
 			this.isBinOp = binop;
@@ -328,6 +385,11 @@ public class CondBreakpoint {
 			return value;
 		}
 	}
+
+	//=========================================================================
+	// These types mirror the Token enum and store some additional information
+	// needed by the parser at later stages
+	//=========================================================================
 
 	static class TokInfo {
 		public Token tok;	
@@ -363,6 +425,10 @@ public class CondBreakpoint {
 	private static TokInfo mkTok(Token t) {
 		return new TokInfo(t);
 	}
+
+	//=========================================================================
+	// Simple scanner to help read the expression string
+	//=========================================================================
 
 	static class SimpleReader {
 		private String input;
@@ -406,20 +472,22 @@ public class CondBreakpoint {
 		}
 	}
 
+	//=========================================================================
+	// Parsing
+	//=========================================================================
+	
 	/**
-	 * Parse a conditional expression the format is as follows:
+	 * Parse a conditional expression the grammar is as follows:
 	 *
-	 * T class.name E
-	 * T := f | m
-	 * E := E and E | E or E | not E | V
-	 * V := 
+	 * E := G and G | G or G 
+	 * G := H > H | H < H | H >= H | H <= H
+	 * H := not E | ( E ) | V
+	 * V := null | integer | string
 	 */
 	public static CondBreakpoint parse(String input) {
 		SimpleReader in = new SimpleReader(input);			
 		try {
 			LinkedList<TokInfo> tokens = new LinkedList<TokInfo>();
-
-			// TODO support fields
 
 			// Start parsing the rest of the tokens
 			String temp = in.next();
@@ -460,27 +528,182 @@ public class CondBreakpoint {
 			// Once we've checked it, create the expression tree
 			Expr root = parseExpression(tokens);
 
-			//System.out.println("Type checking");
 			// Once we've parsed it, make sure everything conforms,
 			// a very simple type check
 			Method m = ClassUtils.getMethod(classAndName[0], classAndName[1]);
 			Class<?>[] types = m.getParameterTypes();
 			if (check(types, root) != Type.BOOL) {
-				throw new Exception("must provide a boolean expression!");
+				throw new Exception("Must provide a boolean expression!");
 			}
 
 			Debugger.println(exprToInfix(root));
 
 			return new CondBreakpoint(classAndName[0] + "." + classAndName[1], root);
 		} catch (Exception e) {
-			Debugger.errorln("Failed to parse conditional: " + 
-				e.getMessage());
-			//e.printStackTrace();
+			Debugger.errorln("Failed to parse conditional: " + e.getMessage());
 		}
 
 		return null;
 	}
 
+	/**
+	 * Does most of the leg work of parsing the string and creating a list of tokens.
+	 *
+	 * @param tokens A list to add tokens to
+	 *
+	 * @param in A scanner that knows the proper delimiters
+	 *
+	 * @throws Exception If an invalid token is found
+	 */
+	private static void parse(LinkedList<TokInfo> tokens, SimpleReader in) throws Exception {
+		while (in.hasNext()) {
+			String val = in.next();
+			//System.out.println("Got token: " + val);
+
+			if (val.equals(Token.LPAREN.value()))   { tokens.add(mkTok(Token.LPAREN)); }
+			else if (val.equals(Token.RPAREN.value())) { tokens.add(mkTok(Token.RPAREN)); }
+			else if (val.equals(Token.AND.value()))  { tokens.add(mkTok(Token.AND));  }
+			else if (val.equals(Token.OR.value()))   { tokens.add(mkTok(Token.OR));   }
+			else if (val.equals(Token.EQ.value()))   { tokens.add(mkTok(Token.EQ));   }
+			else if (val.equals(Token.NOT.value()))  { tokens.add(mkTok(Token.NOT));  }
+			else if (val.equals(Token.GT.value()))   { tokens.add(mkTok(Token.GT));   }
+			else if (val.equals(Token.GTE.value()))  { tokens.add(mkTok(Token.GTE));  }
+			else if (val.equals(Token.LT.value()))   { tokens.add(mkTok(Token.LT));   }
+			else if (val.equals(Token.LTE.value()))  { tokens.add(mkTok(Token.LTE));  }
+			else if (val.equals(Token.NULL.value())) { tokens.add(mkTok(Token.NULL)); }
+			else if (val.equals(Token.ARG.value())) {
+			  	int index = Integer.parseInt(in.next());
+			  	tokens.add(new ArgInfo(index));
+			} else if (Utils.isInteger(val)) {
+				int value = Integer.parseInt(val);
+				tokens.add(new IntInfo(value));
+			} else if (val.startsWith("\"") && val.endsWith("\"")) {
+				tokens.add(new StrInfo(val.substring(1, val.length()-1)));
+			} else {
+				throw new Exception("Invalid token: " + val);
+			}
+		}
+	}
+
+	//=========================================================================
+	// AST creation
+	//=========================================================================
+
+	/**
+	 * This is the method that should be called with a freshly tokenized input.
+	 *
+	 * Note: This is destructive. The tokens list will be empty if successfully
+	 *       converted to an AST.
+	 *
+	 * @param tokens The starting list of tokens to turn into an AST
+	 *
+	 * @throws Exception If it could not correctly create the AST
+	 *
+	 * @return The root node of the AST
+	 */
+	private static Expr parseExpression(LinkedList<TokInfo> tokens) throws Exception {
+		return parseExpression1(tokens, parsePrimary(tokens), 0);
+	}
+
+	/**
+	 * Read a primary value from the tokens list and turn it into an AST node. This
+	 * method also handles parentheses.
+	 *
+	 * Note: This is destructive. The tokens list will be modified.
+	 *
+	 * @param tokens The remaining list of tokens
+	 *
+	 * @throws Exception If a non-primary token is found
+	 */
+	private static Expr parsePrimary(LinkedList<TokInfo> tokens) throws Exception {
+		if (tokens.isEmpty()) {
+			throw new Exception("Parse error");
+		}
+
+		TokInfo ti = tokens.remove(0);
+		if (ti.tok == Token.LPAREN) {
+			Expr e = parseExpression(tokens);
+			ti = tokens.remove(0);
+			if (ti.tok != Token.RPAREN) {
+				throw new Exception("Bad parens");
+			}
+			return e;
+		} else if (ti.tok == Token.INT) {
+			return new IntExpr(((IntInfo)ti).value);
+		} else if (ti.tok == Token.ARG) {
+			return new ArgExpr(((ArgInfo)ti).index);
+		} else if (ti.tok == Token.STR) {
+			return new StrExpr(((StrInfo)ti).value);
+		} else if (ti.tok == Token.NULL) {
+			return new NullExpr();
+		} else if (ti.tok == Token.NOT) {
+			return new NotExpr(parseExpression(tokens));
+		}
+
+		throw new Exception("Parse Primary - Parse Error");
+	}
+
+	/**
+	 * Helper function for correctly parsing (most) precedence based LR(1) grammars. Pseudo code from
+	 * http://en.wikipedia.org/wiki/Operator-precedence_parser
+	 *
+	 * Note: This list removes elements from the tokens list
+	 *
+	 * @param tokens The remaining list of tokens
+	 *
+	 * @param lhs The left hand side expression
+	 *
+	 * @param min_precedence The current minimum precedence, used to determine when to reduce or shift
+	 *
+	 * @return A correctly formed expression tree
+	 */
+	private static Expr parseExpression1(LinkedList<TokInfo> tokens, Expr lhs, int min_precedence) {
+		TokInfo ti = tokens.get(0);
+		while (!tokens.isEmpty() && ti.tok.isBinOp() && ti.tok.precedence() >= min_precedence) {
+			TokInfo op = tokens.remove(0);	
+			Expr rhs = parsePrimary(tokens);
+			if (!tokens.isEmpty()) {
+				ti = tokens.get(0);
+				while (ti.tok.isBinOp() && ti.tok.precedence() > op.tok.precedence()) {
+					TokInfo lookAhead = tokens.get(0);
+					rhs = parseExpression1(tokens, rhs, lookAhead.tok.precedence());
+
+					if (tokens.isEmpty()) break;
+					ti = tokens.get(0);
+				}
+			}
+
+			switch (op.tok) {
+				case AND: lhs = new AndExpr(lhs, rhs); break;
+				case OR:  lhs = new OrExpr(lhs, rhs);  break;
+				case EQ:  lhs = new EqExpr(lhs, rhs);  break;
+				case LT:  lhs = new LtExpr(lhs, rhs);  break;
+				case GT:  lhs = new GtExpr(lhs, rhs);  break;
+				case LTE: lhs = new LteExpr(lhs, rhs); break;
+				case GTE: lhs = new GteExpr(lhs, rhs); break;
+				default:
+					throw new RuntimeException("Unhandled binOp case");
+			}
+
+			if (!tokens.isEmpty()) {
+				ti = tokens.get(0);
+			}
+		}
+
+		return lhs;
+	}
+
+	//=========================================================================
+	// Helper methods
+	//=========================================================================
+
+	/**
+	 * Verify that the parentheses are balanced in the expression.
+	 *
+	 * @param tokens A list of tokens
+	 *
+	 * @return True if the parentheses are balanced, false otherwise
+	 */
 	private static boolean parensMatched(LinkedList<TokInfo> tokens) {
 		int lparens = 0;	
 		for (TokInfo ti : tokens) {
@@ -497,13 +720,14 @@ public class CondBreakpoint {
 		return lparens == 0;
 	}
 
-	enum Type {
-		BOOL,
-		INT,
-		STR,
-		NULL
-	};
-
+	/**
+	 * Convert an expression tree to an infix string representation. Includes all parentheses
+	 * to make the order of operations clear.
+	 *
+	 * @param node The root of the expression tree
+	 *
+	 * @return An infix string of the expression tree
+	 */
 	private static String exprToInfix(Expr node) {
 		switch (node.tok) {
 			case INT:  return "" + ((IntExpr)node).value;
@@ -523,9 +747,31 @@ public class CondBreakpoint {
 		}
 	}
 
+	//=========================================================================
+	// Type checking
+	//=========================================================================
+
+	/**
+	 * The types of the expressions. Used for typechecking
+	 */
+	enum Type {
+		BOOL,
+		INT,
+		STR,
+		NULL
+	};
+
+	/**
+	 * Verifies that the tree is correctly formed with respected to types and
+	 * ensures that argument indices are not out of bounds.
+	 *
+	 * @param types The types of each of the method's arguments
+	 *
+	 * @param node The root node of the expression tree
+	 *
+	 * @return The final type that the expression evaluates to
+	 */
 	private static Type check(Class<?>[] types, Expr node) throws Exception {
-		// We're using the tokens again because I'm lazy
-		// also AND == BOOLEAN type for now
 		switch (node.tok) {
 			case INT: return Type.INT;
 			case STR: return Type.STR;
@@ -601,102 +847,5 @@ public class CondBreakpoint {
 			default:
 				throw new Exception("Unhandled token type!");
 		}
-	}
-
-	private static void parse(LinkedList<TokInfo> tokens, SimpleReader in) throws Exception {
-		while (in.hasNext()) {
-			String val = in.next();
-			//System.out.println("Got token: " + val);
-
-			if (val.equals(Token.LPAREN.value()))   { tokens.add(mkTok(Token.LPAREN)); }
-			else if (val.equals(Token.RPAREN.value())) { tokens.add(mkTok(Token.RPAREN)); }
-			else if (val.equals(Token.AND.value()))  { tokens.add(mkTok(Token.AND));  }
-			else if (val.equals(Token.OR.value()))   { tokens.add(mkTok(Token.OR));   }
-			else if (val.equals(Token.EQ.value()))   { tokens.add(mkTok(Token.EQ));   }
-			else if (val.equals(Token.NOT.value()))  { tokens.add(mkTok(Token.NOT));  }
-			else if (val.equals(Token.GT.value()))   { tokens.add(mkTok(Token.GT));   }
-			else if (val.equals(Token.GTE.value()))  { tokens.add(mkTok(Token.GTE));  }
-			else if (val.equals(Token.LT.value()))   { tokens.add(mkTok(Token.LT));   }
-			else if (val.equals(Token.LTE.value()))  { tokens.add(mkTok(Token.LTE));  }
-			else if (val.equals(Token.NULL.value())) { tokens.add(mkTok(Token.NULL)); }
-			else if (val.equals(Token.ARG.value())) {
-			  	int index = Integer.parseInt(in.next());
-			  	tokens.add(new ArgInfo(index));
-			} else if (Utils.isInteger(val)) {
-				int value = Integer.parseInt(val);
-				tokens.add(new IntInfo(value));
-			} else if (val.startsWith("\"") && val.endsWith("\"")) {
-				tokens.add(new StrInfo(val.substring(1, val.length()-1)));
-			} else {
-				throw new Exception("Invalid token: " + val);
-			}
-		}
-	}
-
-	private static Expr parseExpression(LinkedList<TokInfo> tokens) throws Exception {
-		return parseExpression1(tokens, parsePrimary(tokens), 0);
-	}
-
-	private static Expr parsePrimary(LinkedList<TokInfo> tokens) throws Exception {
-		if (tokens.isEmpty()) {
-			throw new Exception("Parse error");
-		}
-
-		TokInfo ti = tokens.remove(0);
-		if (ti.tok == Token.LPAREN) {
-			Expr e = parseExpression(tokens);
-			ti = tokens.remove(0);
-			if (ti.tok != Token.RPAREN) {
-				throw new Exception("Bad parens");
-			}
-			return e;
-		} else if (ti.tok == Token.INT) {
-			return new IntExpr(((IntInfo)ti).value);
-		} else if (ti.tok == Token.ARG) {
-			return new ArgExpr(((ArgInfo)ti).index);
-		} else if (ti.tok == Token.STR) {
-			return new StrExpr(((StrInfo)ti).value);
-		} else if (ti.tok == Token.NULL) {
-			return new NullExpr();
-		} else if (ti.tok == Token.NOT) {
-			return new NotExpr(parseExpression(tokens));
-		}
-
-		throw new Exception("Parse Primary - Parse Error");
-	}
-
-	private static Expr parseExpression1(LinkedList<TokInfo> tokens, Expr lhs, int min_precedence) throws Exception {
-		TokInfo ti = tokens.get(0);
-		while (!tokens.isEmpty() && ti.tok.isBinOp() && ti.tok.precedence() >= min_precedence) {
-			TokInfo op = tokens.remove(0);	
-			Expr rhs = parsePrimary(tokens);
-			if (!tokens.isEmpty()) {
-				ti = tokens.get(0);
-				while (ti.tok.isBinOp() && ti.tok.precedence() > op.tok.precedence()) {
-					TokInfo lookAhead = tokens.get(0);
-					rhs = parseExpression1(tokens, rhs, lookAhead.tok.precedence());
-
-					if (tokens.isEmpty()) break;
-					ti = tokens.get(0);
-				}
-			}
-			switch (op.tok) {
-				case AND: lhs = new AndExpr(lhs, rhs); break;
-				case OR:  lhs = new OrExpr(lhs, rhs);  break;
-				case EQ:  lhs = new EqExpr(lhs, rhs);  break;
-				case LT:  lhs = new LtExpr(lhs, rhs);  break;
-				case GT:  lhs = new GtExpr(lhs, rhs);  break;
-				case LTE: lhs = new LteExpr(lhs, rhs); break;
-				case GTE: lhs = new GteExpr(lhs, rhs); break;
-				default:
-					throw new Exception("Unhandled binOp case");
-			}
-
-			if (!tokens.isEmpty()) {
-				ti = tokens.get(0);
-			}
-		}
-
-		return lhs;
 	}
 }
